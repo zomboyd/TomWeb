bb651c7cb25452744cc0652ca6f7ad189df40b5f
zomboyd <alecko.bc@gmail.com>
2015-12-16 12:43:29 +0100

    first commit

---
+++ javabeans/jswebModel.java
@@ -0,0 +1,16 @@
+package jsweb;
+
+public class jswebModel implements java.io.Serializable {
+  private String name = null;
+
+  public jswebModel() {
+  }
+
+  public String getName() {
+    return (name);
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
